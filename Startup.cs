f3edb93eecc2b8fddb46ce6a5bc740b8e14ded64
Tian <nevillett8@gmail.com>
2018-09-28 15:13:35 -0400

    DbContest + EFCore Desing, migration, initial database

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SPA_Angular.NETCore.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace SPA_Angular.NETCore
 {
@@ -22,6 +24,7 @@ namespace SPA_Angular.NETCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SpaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
         }
 
