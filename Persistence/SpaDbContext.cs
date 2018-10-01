dcc1c93050bb9453b1dfa4cbebc5b926df374f4a
Tian <nevillett8@gmail.com>
2018-10-01 17:41:44 -0400

    adding dbset<vehicle>

--- Persistence/SpaDbContext.cs
+++ Persistence/SpaDbContext.cs
@@ -7,6 +7,7 @@ namespace SPA_Angular.NETCore.Persistence
     {
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
         public SpaDbContext(DbContextOptions<SpaDbContext> options) : base(options)
         {
         }
